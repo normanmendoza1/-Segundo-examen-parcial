2c522bdc49360b261110ab7c2d4155dfb762f2ed
normanmendoza1 <vallejosnorman9@gmail.com>
2026-07-03 03:25:48 +0200

    Create Book model with attributes

---
+++ lib/models/book.dart
@@ -0,0 +1,14 @@
+// Modelo que representa un libro.
+class Book {
+  String title;
+  String author;
+  int pages;
+  bool isFiction;
+
+  Book({
+    required this.title,
+    required this.author,
+    required this.pages,
+    required this.isFiction,
+  });
+}
